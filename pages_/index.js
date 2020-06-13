47d3c83aa8f821065c4cfafc155bb7b8b7bf0878
ay8s <contact@andydev.co.uk>
2020-06-13 12:57:29 -0700

    Test Canary next-translate for static indexes

--- pages_/index.js
+++ pages_/index.js
@@ -1,20 +1,21 @@
-import Router from 'next/router'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
 import Layout from '../components/Layout';
 import Races from '../components/Races';
-import { NextSeo } from 'next-seo';
+import {NextSeo} from 'next-seo';
 import Link from 'next/link';
 import RaceSchema from '../components/RaceSchema';
 import useTranslation from 'next-translate/useTranslation'
 
-const Index = (props) => {				
-  const { t, lang } = useTranslation()
-  const title = t('common:title')
-  const subtitle = t('common:subtitle')
-  
-  const currentYear = '2020';
-  const metaDescription = t('common:meta.description', { year: currentYear })
-  const metaKeywords = t('common:meta.keywords', { year: currentYear })
-  
+const Index = (props) => {
+	const {t, lang} = useTranslation()
+	const title = t('common:title')
+	const subtitle = t('common:subtitle')
+
+	const currentYear = '2020';
+	const metaDescription = t('common:meta.description', {year: currentYear})
+	const metaKeywords = t('common:meta.keywords', {year: currentYear})
+
 	return (
 		<>
 			<NextSeo
@@ -22,37 +23,36 @@ const Index = (props) => {
 				description={metaDescription}
 				keywords={metaKeywords}
 			/>
-	    <Layout showOptions='true' showCalendarExport='true' year={ props.year }>
-				<Races year={ props.year } races={ props.races } />
+			<Layout showOptions='true' showCalendarExport='true' year={props.year}>
+				<Races year={props.year} races={props.races}/>
 				{props.races && props.races.map((item, index) => {
-					if(item.sessions){
-  					return (<RaceSchema item={item} key={item.name} />)
+					if (item.sessions) {
+						return (<RaceSchema item={item} key={item.name}/>)
 					}
 				})}
-	    </Layout>
-    </>
+			</Layout>
+		</>
 	);
 }
 
-Index.getInitialProps = async ({query: {timezone}, res}) => {
-  const currentYear = '2020';
+export default Index;
 
-	const data = await import(`../db/`+currentYear+`.json`)
-	
-	// Handle cases where we're not able to automatically switch dates/times (noscript)
-	if(timezone){
-		res.writeHead(302, {
-			Location: '/timezone/'+timezone.replace("/", "-")
-		})
-		res.end()
-		return;
-	}
-	
-	return {
-	    year: currentYear,
-	    races: data.races,
-	    virtual: data.virtual
-	}
+export const getStaticPaths = async () => {
+	return ({
+		paths: [],
+		fallback: false,
+	})
 }
 
-export default Index;
+export const getStaticProps = async ({params}) => {
+	const currentYear = '2020';
+
+	const data = await import(`../db/` + currentYear + `.json`)
+
+	return {
+		props: {
+			year: currentYear,
+			races: data.races
+		}
+	}
+};
